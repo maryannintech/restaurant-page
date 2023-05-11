94a5d1b2c6757341377b050299ecddc4daa5d940
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-05-11 11:44:09 +0800

    changes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,50 @@ import homeContent from "./modules/home";
 import menuContent from "./modules/menu";
 import contactContent from "./modules/contact";
 
+let homeClicked = false;
+let menuClicked = false;
+let contactClicked = false;
+
+const contentDIV = document.querySelector("#content");
+
 homeContent();
+
+const homePage = document.querySelector(".home");
+const menuPage = document.querySelector(".menu");
+const contactPage = document.querySelector(".contact");
+
+const homeBtn = document.querySelector(".homeBtn");
+homeBtn.addEventListener("click", () => {
+  contentDIV.appendChild(homePage);
+  if (homeClicked === false) {
+    homeContent();
+    homeClicked = true;
+  } else {
+  }
+  menuClicked = false;
+  contactClicked = false;
+});
 
 const menuBtn = document.querySelector(".menuBtn");
 menuBtn.addEventListener("click", () => {
-  menuContent();
+  contentDIV.removeChild(homePage);
+  if (menuClicked === false) {
+    menuContent();
+    menuClicked = true;
+  } else {
+  }
+  homeClicked = false;
+  contactClicked = false;
 });
 
 const contactBtn = document.querySelector(".contactBtn");
 contactBtn.addEventListener("click", () => {
-  contactContent();
+  contentDIV.removeChild(homePage);
+  if (contactClicked === false) {
+    contactContent();
+    contactClicked = true;
+  } else {
+  }
+  homeClicked = false;
+  menuClicked = false;
 });
