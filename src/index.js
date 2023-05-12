b0affc2be206de0080c28331191e09580c79045a
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-05-12 16:16:21 +0800

    changes

--- src/index.js
+++ src/index.js
@@ -23,39 +23,63 @@ menuPage.classList.add("menu");
 const contactPage = document.createElement("div");
 contactPage.classList.add("contact");
 
+// Add event listeners to the home, menu, and contact buttons
 homeBTN.addEventListener("click", () => {
+  // If the home button is not disabled and the current page is not home
   if (!homeBTN.disabled && displayed !== "home") {
+    // Show the home content
     homeContent();
+    // Set the displayed variable to home
     displayed = "home";
+    // Disable the home button
     homeBTN.disabled = true;
-    contactPage.classList.add("hide");
+    // Enable the menu and contact buttons
+    menuBTN.disabled = false;
+    contactBTN.disabled = false;
+    // Hide the menu and contact pages
     menuPage.classList.add("hide");
+    contactPage.classList.add("hide");
+    // Show the home page
     homePage.classList.remove("hide");
   }
 });
 
 menuBTN.addEventListener("click", () => {
+  // If the menu button is not disabled and the current page is not menu
   if (!menuBTN.disabled && displayed !== "menu") {
+    // Show the menu content
     menuContent();
+    // Set the displayed variable to menu
     displayed = "menu";
+    // Disable the menu button
     menuBTN.disabled = true;
+    // Enable the home and contact buttons
     homeBTN.disabled = false;
     contactBTN.disabled = false;
+    // Hide the home and contact pages
     homePage.classList.add("hide");
     contactPage.classList.add("hide");
-    menuPage.classList.remove("hide");;
+    // Show the menu page
+    menuPage.classList.remove("hide");
   }
 });
 
 contactBTN.addEventListener("click", () => {
+  // If the contact button is not disabled and the current page is not contact
   if (!contactBTN.disabled && displayed !== "contact") {
+    // Show the contact content
     contactContent();
+    // Set the displayed variable to contact
     displayed = "contact";
+    // Disable the contact button
     contactBTN.disabled = true;
+    // Enable the home and menu buttons
     homeBTN.disabled = false;
     menuBTN.disabled = false;
+    // Hide the home and menu pages
     homePage.classList.add("hide");
     menuPage.classList.add("hide");
+    // Show the contact page
     contactPage.classList.remove("hide");
   }
 });
