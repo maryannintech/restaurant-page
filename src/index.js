2962990c915b2c88fb79b4e72bfe08f99fc3294e
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-05-12 12:01:24 +0800

    changes

--- src/index.js
+++ src/index.js
@@ -12,51 +12,48 @@ const contentDIV = document.querySelector("#content");
 
 makeNav();
 homeContent();
-menuContent();
-contactContent();
 
 const homePage = document.querySelector(".home");
-console.log(homePage);
 const menuPage = document.querySelector(".menu");
-console.log(menuPage);
 const contactPage = document.querySelector(".contact");
-console.log(contactPage);
 
-const homeBtn = document.querySelector(".homeBtn");
-homeBtn.addEventListener("click", () => {
-  if (homeClicked === false) {
-    homeContent();
+function init() {
+  const homeBtn = document.querySelector(".homeBtn");
+  homeBtn.addEventListener("click", () => {
+    if (homeClicked === false) {
+      homeContent();
+      homeClicked = false;
+      menuPage.style.display = "none";
+      contactPage.style.display = "none";
+    }
+    menuClicked = false;
+    contactClicked = false;
+  });
+
+  const menuBtn = document.querySelector(".menuBtn");
+  menuBtn.addEventListener("click", () => {
+    if (menuClicked === false) {
+      menuContent();
+      menuClicked = true;
+      homePage.style.display = "none";
+      contactPage.style.display = "none";
+    }
     homeClicked = false;
-    menuPage.style.display = "none";
-    contactPage.style.display = "none";
-  } else {
-  }
-  menuClicked = false;
-  contactClicked = false;
-});
+    contactClicked = false;
+  });
 
-const menuBtn = document.querySelector(".menuBtn");
-menuBtn.addEventListener("click", () => {
-  if (menuClicked === false) {
-    menuContent();
-    menuClicked = true;
-    homePage.style.display = "none";
-    contactPage.style.display = "none";
-  } else {
-  }
-  homeClicked = false;
-  contactClicked = false;
-});
+  const contactBtn = document.querySelector(".contactBtn");
+  contactBtn.addEventListener("click", () => {
+    if (contactClicked === false) {
+      contactContent();
+      contactClicked = true;
+      homePage.style.display = "none";
+      menuPage.style.display = "none";
+    } else {
+    }
+    homeClicked = false;
+    menuClicked = false;
+  });
+}
 
-const contactBtn = document.querySelector(".contactBtn");
-contactBtn.addEventListener("click", () => {
-  if (contactClicked === false) {
-    contactContent();
-    contactClicked = true;
-    homePage.style.display = "none";
-    menuPage.style.display = "none";
-  } else {
-  }
-  homeClicked = false;
-  menuClicked = false;
-});
+init();
