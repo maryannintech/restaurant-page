e5491dd47152d5b8b48f044567e998d8cdb50a03
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-05-12 15:10:38 +0800

    try to fix logic

--- src/index.js
+++ src/index.js
@@ -6,21 +6,26 @@ import makeNav from "./modules/nav";
 
 makeNav();
 homeContent();
+
 let displayed = "home";
 
 const contentDIV = document.querySelector("#content");
 const homeBTN = document.querySelector(".homeBtn");
 const menuBTN = document.querySelector(".menuBtn");
 const contactBTN = document.querySelector(".contactBtn");
+
 const homePage = document.querySelector(".home");
 const menuPage = document.querySelector(".menu");
 const contactPage = document.querySelector(".contact");
 
 homeBTN.addEventListener("click", () => {
   if (!homeBTN.disabled && displayed !== "home") {
-    displayContent();
+    homeContent();
     displayed = "home";
     homeBTN.disabled = true;
+    contactPage.classList.add("hide");
+    menuPage.classList.add("hide");
+    homePage.style.display = "block";
   }
 });
 
@@ -31,6 +36,9 @@ menuBTN.addEventListener("click", () => {
     menuBTN.disabled = true;
     homeBTN.disabled = false;
     contactBTN.disabled = false;
+    homePage.classList.add("hide");
+    contactPage.classList.add("hide");
+    menuPage.style.display = "block";
   }
 });
 
@@ -41,15 +49,8 @@ contactBTN.addEventListener("click", () => {
     contactBTN.disabled = true;
     homeBTN.disabled = false;
     menuBTN.disabled = false;
+    homePage.classList.add("hide");
+    menuPage.classList.add("hide");
+    contactPage.style.disabled = "block"
   }
 });
-
-function displayContent() {
-  if (displayed === "home") {
-    homeContent();
-  } else if (displayed === "menu") {
-    menuContent();
-  } else if (displayed === "contact") {
-    contactContent();
-  }
-}
